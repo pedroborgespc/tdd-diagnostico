fb91503bc77a12017321a26df22910fce8f1e9ab
Pedro Borges <pedroborgespc@outlook.com>
2019-12-17 20:45:21 -0300

    resolucao2.py

--- resolucao2.py
+++ resolucao2.py
@@ -1,4 +1,15 @@
-s = str(input('Digite seu texto:'))
+mainString = print(input('Digite seu texto:'))
 
-str_new = s.replace('a', '@')
-print(str_new)
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces:
+        # Check if string is in the main string
+        if elem in mainString:
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+
+    return mainString
+
+print(mainString)
+
+
