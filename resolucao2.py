0cdd42bff3186c5f63bd1516ab18bf6ace1f220e
Pedro Borges <pedroborgespc@outlook.com>
2019-12-17 20:48:14 -0300

    resolucao2.py

--- resolucao2.py
+++ resolucao2.py
@@ -1,9 +1,9 @@
 mainString = print(input('Digite seu texto:'))
 
-def replaceMultiple(string_principal, substituir, newString):
-    for elem in substituir:
-        if elem in string_principal:
-            string_principal = string_principal.replace(elem, newString)
+def replaceMultiple(string_principal, substituir, nova_string):
+    for elemento in substituir:
+        if elemento in string_principal:
+            string_principal = string_principal.replace(elemento, nova_string)
 
     return string_principal
 
